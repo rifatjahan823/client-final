cfaf8ca5cc14a2bd583d35cb54d20589f327a17f
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-05-14 16:24:45 +0600

    query set

--- index.js
+++ index.js
@@ -12,29 +12,56 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hnowq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const servicesCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
 
-        app.get('/service', async (req, res)=>{
+        app.get('/service', async (req, res) => {
             const query = {};
-            const  cursor= servicesCollection.find(query);
+            const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
+
+
+    /**
+        * API Naming Convention
+        * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+        * app.get('/booking/:id') // get a specific booking 
+        * app.post('/booking') // add a new booking
+        * app.patch('/booking/:id) //
+        * app.delete('/booking/:id) //
+    */
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient};
+            const bookings = await bookingCollection.insertOne(booking);
+            res.send(bookings);
+        })
+
+        app.get('/booking', async (req, res) => {
+            const query = {};
+            const cursor = bookingCollection.find(query);
+            const bookings = await cursor.toArray();
+            res.send(bookings);
+        })
+
         console.log('connected to db');
     }
-    finally{
+
+    finally {
 
     }
 }
 run().catch(console.dir)
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Running doctors portal server')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Doctors app listening on port', port);
 })
