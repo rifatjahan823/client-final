23d1076e4ecf8182edcda449dfa2e4a8a076d7a2
rifatjahan823 <rifatjahan823@gmail.com>
2022-09-17 20:22:16 +0600

    add

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hnowq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wuigzgs.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function verifyJWT(req, res, next) {
